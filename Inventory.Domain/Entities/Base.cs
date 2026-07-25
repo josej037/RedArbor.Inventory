96668df576582119d73be5caeb5787103c31f2e7
Jose Guzman <josej_1037@hotmail.com>
2026-07-24 23:11:00 -0600

    Initial Structure

---
+++ Inventory.Domain/Entities/Base.cs
@@ -0,0 +1,11 @@
+﻿namespace Inventory.Domain.Entities
+{
+    public abstract class Base
+    {
+        public int Id { get; set; }
+        public bool active { get; set; }
+        public int UserId { get; set; }
+        public DateTime CreatedAt { get; set; }
+        public DateTime UpdatedAt { get; set; }
+    }
+}
